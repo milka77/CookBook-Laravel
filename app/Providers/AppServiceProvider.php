5a5583362942f630ff45efef576cc4366dc905a9
milka77 <55999046+milka77@users.noreply.github.com>
2022-05-13 18:58:01 +0100

    changed appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,9 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if (env('APP_ENV') !== 'local') {
-            URL::forceSchema('https');
-        }
+        if (env('APP_ENV') === 'production') {
+            $this->app['request']->server->set('HTTPS', true);
+        };
         Paginator::useBootstrap();
     }
 }
