dfd2fd62c9305cc21627c08353aa104b08620ba8
milka77 <55999046+milka77@users.noreply.github.com>
2022-05-13 18:34:34 +0100

    Changed git ignore file and moved site-images to /site-images folder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if (env('APP_ENV') !== 'local') {
+            URL::forceSchema('https');
+        }
         Paginator::useBootstrap();
-        URL::forceSchema('https');
     }
 }
