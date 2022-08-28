051528a2f61e055c09bb6212b5b25ec525ade067
milka77 <55999046+milka77@users.noreply.github.com>
2022-08-28 15:50:43 +0100

    Moved the Roles routes to a seperate file roles.php @ routes #2

--- routes/web/web.php
+++ routes/web/web.php
@@ -29,14 +29,6 @@ Route::middleware('auth')->prefix('admin')->group(function(){
     Route::get('/', [AdminController::class, 'index'])->name('admin.index');
     Route::get('/allrecipe', [AdminController::class, 'allRecipies'])->name('admin.allrecipies');
 
-    Route::get('/role', [RoleController::class, 'index'])->name('role.index');
-    Route::get('/role/new', [RoleController::class, 'create'])->name('role.create');
-    Route::post('/role', [RoleController::class, 'store'])->name('role.store');
-
-    Route::get('/role/{role}/edit', [RoleController::class, 'edit'])->name('role.edit');
-    Route::patch('/role/{role}/update', [RoleController::class, 'update'])->name('role.update');
-
-    Route::delete('/role/{role}/destroy', [RoleController::class, 'destroy'])->name('role.destroy');
 
 });
 
